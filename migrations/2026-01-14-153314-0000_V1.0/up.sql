73e46d6957cf011c97282953cee5c5d47740b12c
Philip-Bachl <p.bachl@icloud.com>
2026-01-15 08:43:29 +0100

    nullable fix

--- migrations/2026-01-14-153314-0000_V1.0/up.sql
+++ migrations/2026-01-14-153314-0000_V1.0/up.sql
@@ -1,47 +1,47 @@
 -- Your SQL goes here
 
 CREATE TABLE ge_gebaeude (
-    ge_name TEXT,
+    ge_name TEXT NOT NULL,
     PRIMARY KEY (ge_name)
 );
 
 CREATE TABLE ra_raum (
-    ra_ge_name TEXT REFERENCES ge_name,
-    ra_stockwerk INTEGER,
-    ra_nummer TEXT,
+    ra_ge_name TEXT REFERENCES ge_name NOT NULL,
+    ra_stockwerk INTEGER NOT NULL,
+    ra_nummer TEXT NOT NULL,
     PRIMARY KEY (ra_ge_name, ra_stockwerk, ra_nummer)
 );
 
 CREATE TABLE sc_schrank (
-    sc_nummer INTEGER,
-    sc_stockwerk INTEGER,
-    sc_ge_name TEXT,
+    sc_nummer INTEGER NOT NULL,
+    sc_stockwerk INTEGER NOT NULL,
+    sc_ge_name TEXT NOT NULL,
     PRIMARY KEY (sc_nummer, sc_stockwerk, sc_ge_name)
 );
 
 CREATE TABLE do_dose (
-    do_nummer TEXT,
-    do_ra_nummer INTEGER REFERENCES ra_nummer,
-    do_ra_stockwerk INTEGER REFERENCES ra_stockwerk,
+    do_nummer TEXT NOT NULL,
+    do_ra_nummer INTEGER REFERENCES ra_nummer NOT NULL,
+    do_ra_stockwerk INTEGER REFERENCES ra_stockwerk NOT NULL,
     PRIMARY KEY (do_nummer, do_ra_nummer, do_ra_stockwerk)
 );
 
 CREATE TABLE sw_switch (
-    sw_ip TEXT,
-    sw_sc_nummer INTEGER REFERENCES sc_nummer,
-    sw_sc_stockwerk INTEGER REFERENCES sc_stockwerk,
+    sw_ip TEXT NOT NULL,
+    sw_sc_nummer INTEGER REFERENCES sc_nummer NOT NULL,
+    sw_sc_stockwerk INTEGER REFERENCES sc_stockwerk NOT NULL,
     PRIMARY KEY (sw_ip)
 );
 
 CREATE TABLE sd_switch_zu_dose (
-    sd_sw_ip INTEGER REFERENCES sw_ip,
-    sd_do_nummer TEXT REFERENCES do_nummer,
-    sd_do_ra_nummer INTEGER REFERENCES do_ra_nummer,
-    sd_do_ra_stockwerk INTEGER REFERENCES do_ra_stockwerk,
-    sd_switchport TEXT,
-    sd_hat_telefon INTEGER,
-    sd_hat_pc INTEGER,
-    sd_hat_drucker INTEGER,
+    sd_sw_ip INTEGER REFERENCES sw_ip NOT NULL,
+    sd_do_nummer TEXT REFERENCES do_nummer NOT NULL,
+    sd_do_ra_nummer INTEGER REFERENCES do_ra_nummer NOT NULL,
+    sd_do_ra_stockwerk INTEGER REFERENCES do_ra_stockwerk NOT NULL,
+    sd_switchport TEXT NOT NULL,
+    sd_hat_telefon INTEGER NOT NULL,
+    sd_hat_pc INTEGER NOT NULL,
+    sd_hat_drucker INTEGER NOT NULL,
     sd_kommentar TEXT,
     PRIMARY KEY (sd_sw_ip, sd_do_nummer, sd_do_ra_nummer, sd_do_ra_stockwerk)
 );
--- src/schema.rs
+++ src/schema.rs
@@ -2,53 +2,53 @@
 
 diesel::table! {
     do_dose (do_nummer, do_ra_nummer, do_ra_stockwerk) {
-        do_nummer -> Nullable<Text>,
-        do_ra_nummer -> Nullable<Integer>,
-        do_ra_stockwerk -> Nullable<Integer>,
+        do_nummer -> Text,
+        do_ra_nummer -> Integer,
+        do_ra_stockwerk -> Integer,
     }
 }
 
 diesel::table! {
     ge_gebaeude (ge_name) {
-        ge_name -> Nullable<Text>,
+        ge_name -> Text,
     }
 }
 
 diesel::table! {
     ra_raum (ra_ge_name, ra_stockwerk, ra_nummer) {
-        ra_ge_name -> Nullable<Text>,
-        ra_stockwerk -> Nullable<Integer>,
-        ra_nummer -> Nullable<Text>,
+        ra_ge_name -> Text,
+        ra_stockwerk -> Integer,
+        ra_nummer -> Text,
     }
 }
 
 diesel::table! {
     sc_schrank (sc_nummer, sc_stockwerk, sc_ge_name) {
-        sc_nummer -> Nullable<Integer>,
-        sc_stockwerk -> Nullable<Integer>,
-        sc_ge_name -> Nullable<Text>,
+        sc_nummer -> Integer,
+        sc_stockwerk -> Integer,
+        sc_ge_name -> Text,
     }
 }
 
 diesel::table! {
     sd_switch_zu_dose (sd_sw_ip, sd_do_nummer, sd_do_ra_nummer, sd_do_ra_stockwerk) {
-        sd_sw_ip -> Nullable<Integer>,
-        sd_do_nummer -> Nullable<Text>,
-        sd_do_ra_nummer -> Nullable<Integer>,
-        sd_do_ra_stockwerk -> Nullable<Integer>,
-        sd_switchport -> Nullable<Text>,
-        sd_hat_telefon -> Nullable<Integer>,
-        sd_hat_pc -> Nullable<Integer>,
-        sd_hat_drucker -> Nullable<Integer>,
+        sd_sw_ip -> Integer,
+        sd_do_nummer -> Text,
+        sd_do_ra_nummer -> Integer,
+        sd_do_ra_stockwerk -> Integer,
+        sd_switchport -> Text,
+        sd_hat_telefon -> Integer,
+        sd_hat_pc -> Integer,
+        sd_hat_drucker -> Integer,
         sd_kommentar -> Nullable<Text>,
     }
 }
 
 diesel::table! {
     sw_switch (sw_ip) {
-        sw_ip -> Nullable<Text>,
-        sw_sc_nummer -> Nullable<Integer>,
-        sw_sc_stockwerk -> Nullable<Integer>,
+        sw_ip -> Text,
+        sw_sc_nummer -> Integer,
+        sw_sc_stockwerk -> Integer,
     }
 }
 
