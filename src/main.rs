989fa868a9fa9d750f534453b7f9b2ae14fcf543
Philip-Bachl <p.bachl@icloud.com>
2026-01-15 10:54:56 +0100

    added database struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use std::env;
+
+use dotenvy::dotenv;
+
+use crate::model::Masterbase;
+
 pub mod model;
 pub mod schema;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv()?;
+
+    let connection_string = env::var("DATABASE_URL")?;
+    let database = Masterbase::connect(&connection_string);
+
+    Ok(())
 }
--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,16 @@
-use diesel::{Selectable, SqliteConnection, prelude::Queryable};
+use diesel::{Connection, ConnectionError, Selectable, SqliteConnection, prelude::Queryable};
 
 use crate::schema;
+
+pub struct Masterbase {
+    connection: SqliteConnection,
+}
+
+impl Masterbase {
+    pub fn connect(connection_string: &str) -> Result<Masterbase, ConnectionError> {
+        SqliteConnection::establish(connection_string).map(|connection| Masterbase { connection })
+    }
+}
 
 #[derive(Queryable, Selectable)]
 #[diesel(table_name = schema::do_dose)]
