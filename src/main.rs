b681f942d992cc20937cfc6125ae4c7d3a687765
Philip-Bachl <p.bachl@icloud.com>
2026-01-26 14:33:19 +0100

    added missing gebaeude endpoints

--- src/main.rs
+++ src/main.rs
@@ -7,56 +7,15 @@ use std::{
 use dotenvy::dotenv;
 
 use masterbase::Masterbase;
-use rocket::{State, get, http::Status, launch, post, routes, serde::json::Json};
+use rocket::{launch, routes};
 
-use crate::model::Gebaeude;
-
+mod endpoints;
 mod masterbase;
 mod model;
 mod schema;
 
 struct MasterState {
     database: Arc<Mutex<Masterbase>>,
-}
-
-//TODO: error handling with custom type
-
-#[get("/gebaeude")]
-async fn get_gebaeude_all(state: &State<MasterState>) -> Result<Json<Vec<Gebaeude>>, Status> {
-    let Ok(mut database) = state.database.lock() else {
-        println!("ERROR: Poisioned Lock detected at 'get_gebaeude_all'");
-        return Err(Status::InternalServerError);
-    };
-
-    let gebaeude_all = match database.read_gebaeude_all() {
-        Ok(ge_al) => ge_al,
-        Err(err) => {
-            println!("ERROR: at 'get_gebaeude_all': {err}");
-            return Err(Status::InternalServerError);
-        }
-    };
-
-    Ok(Json(gebaeude_all))
-}
-
-//TODO: add read endpoints with filters
-
-#[post("/gebaeude", data = "<gebaeude>")]
-async fn add_gebaede(state: &State<MasterState>, gebaeude: Json<Gebaeude>) -> Status {
-    let Ok(mut database) = state.database.lock() else {
-        println!("ERROR: Poisioned Lock detected at 'add_gebaeude'");
-        return Status::InternalServerError;
-    };
-
-    let gebaeude = gebaeude.0;
-
-    match database.create_gebaeude(gebaeude) {
-        Ok(_) => Status::Created,
-        Err(err) => {
-            println!("ERROR: at 'add_gebaeude': {err}");
-            Status::InternalServerError
-        }
-    }
 }
 
 #[launch]
@@ -82,7 +41,14 @@ async fn rocket() -> _ {
         .manage(MasterState {
             database: Arc::new(Mutex::new(database)),
         })
-        .mount("/db", routes![get_gebaeude_all, add_gebaede]);
+        .mount(
+            "/db",
+            routes![
+                endpoints::get_gebaeude_all,
+                endpoints::add_gebaede,
+                endpoints::change_gebaede
+            ],
+        );
 
     for r in app.routes() {
         println!("{r}");
