56eeb6b4bf13ccc987f9cefc9909dc7284acd4ae
Philip-Bachl <p.bachl@icloud.com>
2026-01-28 16:32:28 +0100

    fixed model

--- db_scripts/up.sql
+++ db_scripts/up.sql
@@ -6,21 +6,22 @@ CREATE TABLE ge_gebaeude (
 );
 
 CREATE TABLE ra_raum (
-    ra_ge_name TEXT REFERENCES ge_name NOT NULL,
-    ra_stockwerk INTEGER NOT NULL,
     ra_nummer TEXT NOT NULL,
+    ra_stockwerk INTEGER NOT NULL,
+    ra_ge_name TEXT REFERENCES ge_name NOT NULL,
     PRIMARY KEY (ra_ge_name, ra_stockwerk, ra_nummer)
 );
 
 CREATE TABLE sc_schrank (
     sc_nummer INTEGER NOT NULL,
     sc_stockwerk INTEGER NOT NULL,
-    sc_ge_name TEXT NOT NULL,
+    sc_ge_name TEXT REFERENCES ge_name NOT NULL,
     PRIMARY KEY (sc_nummer, sc_stockwerk, sc_ge_name)
 );
 
 CREATE TABLE do_dose (
     do_nummer TEXT NOT NULL,
+    do_ra_ge_name TEXT REFERENCES ra_ge_name NOT NULL,
     do_ra_nummer INTEGER REFERENCES ra_nummer NOT NULL,
     do_ra_stockwerk INTEGER REFERENCES ra_stockwerk NOT NULL,
     PRIMARY KEY (do_nummer, do_ra_nummer, do_ra_stockwerk)
@@ -28,8 +29,9 @@ CREATE TABLE do_dose (
 
 CREATE TABLE sw_switch (
     sw_ip TEXT NOT NULL,
-    sw_sc_nummer INTEGER REFERENCES sc_nummer NOT NULL,
-    sw_sc_stockwerk INTEGER REFERENCES sc_stockwerk NOT NULL,
+    sw_sc_nummer INTEGER NOT NULL,
+    sw_sc_stockwerk INTEGER NOT NULL,
+    sw_sc_ge_name TEXT REFERENCES sc_ge_name NOT NULL,
     PRIMARY KEY (sw_ip)
 );
 
@@ -38,6 +40,7 @@ CREATE TABLE sd_switch_zu_dose (
     sd_do_nummer TEXT REFERENCES do_nummer NOT NULL,
     sd_do_ra_nummer INTEGER REFERENCES do_ra_nummer NOT NULL,
     sd_do_ra_stockwerk INTEGER REFERENCES do_ra_stockwerk NOT NULL,
+    sd_do_ra_ge_name TEXT REFERENCES do_ra_ge_name NOT NULL,
     sd_switchport TEXT NOT NULL,
     sd_hat_telefon BOOLEAN NOT NULL,
     sd_hat_pc BOOLEAN NOT NULL,
--- src/model.rs
+++ src/model.rs
@@ -1,21 +1,16 @@
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
 
-pub struct Dose {
-    pub do_nummer: String,
-    pub do_ra_nummer: i32,
-    pub do_ra_stockwerk: i32,
-}
-
 #[derive(Serialize, Deserialize, FromRow)]
 pub struct Gebaeude {
     pub ge_name: String,
 }
 
+#[derive(Serialize, Deserialize, FromRow)]
 pub struct Raum {
-    pub ra_ge_name: String,
-    pub ra_stockwerk: i32,
     pub ra_nummer: String,
+    pub ra_stockwerk: i32,
+    pub ra_ge_name: String,
 }
 
 pub struct Schrank {
@@ -24,20 +19,29 @@ pub struct Schrank {
     pub sc_ge_name: String,
 }
 
-pub struct SwitchZuDose {
-    pub sd_sw_ip: String,
-    pub sd_do_nummer: String,
-    pub sd_do_ra_nummer: i32,
-    pub sd_do_ra_stockwerk: i32,
-    pub sd_switchport: String,
-    pub sd_hat_telefon: bool,
-    pub sd_hat_pc: bool,
-    pub sd_hat_drucker: bool,
-    pub sd_kommentar: Option<String>,
+pub struct Dose {
+    pub do_nummer: String,
+    pub do_ra_ge_name: String,
+    pub do_ra_nummer: i32,
+    pub do_ra_stockwerk: i32,
 }
 
 pub struct Switch {
     pub sw_ip: String,
     pub sw_sc_nummer: i32,
     pub sw_sc_stockwerk: i32,
+    pub sw_sc_ge_name: i32,
+}
+
+pub struct SwitchZuDose {
+    pub sd_sw_ip: String,
+    pub sd_do_nummer: String,
+    pub sd_do_ra_nummer: i32,
+    pub sd_do_ra_stockwerk: i32,
+    pub sd_do_ra_ge_name: String,
+    pub sd_switchport: String,
+    pub sd_hat_telefon: bool,
+    pub sd_hat_pc: bool,
+    pub sd_hat_drucker: bool,
+    pub sd_kommentar: Option<String>,
 }
